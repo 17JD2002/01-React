94a4cbbb4870d25ca494370e8c7119c48f51d3d3
17JD2002 <jdc16315@gmail.com>
2022-03-30 01:04:21 -0500

    REACT-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Aprendiendo REACT</h1>
+      </div>
+      <Testimonio
+        nombre='Darwin'
+        imagen='darwin'
+        cargo='Personaje de Ficcion'
+        empresa='Cartoon Network'
+        testimonio='Darwin Raglan Caspian Ahab Poseidon Nicodemius Watterson III es uno de los personajes principales de El Increíble Mundo de Gumball. Él solía ser una mascota de Gumball, pero un día a él le creció piernas, ganó la capacidad de hablar, y en general se adaptó a estar fuera del agua'/>
+      <Testimonio 
+      nombre='Jesus'
+      imagen='bart'
+      cargo='Personaje de Ficcion'
+      empresa='Cartoon Network'
+      testimonio='Darwin Raglan Caspian Ahab Poseidon Nicodemius Watterson III es uno de los personajes principales de El Increíble Mundo de Gumball. Él solía ser una mascota de Gumball, pero un día a él le creció piernas, ganó la capacidad de hablar, y en general se adaptó a estar fuera del agua'/>
+      <Testimonio
+      nombre='Charris'
+      imagen='tortuga'
+      cargo='Personaje de Ficcion'
+      empresa='Cartoon Network'
+      testimonio='Darwin Raglan Caspian Ahab Poseidon Nicodemius Watterson III es uno de los personajes principales de El Increíble Mundo de Gumball. Él solía ser una mascota de Gumball, pero un día a él le creció piernas, ganó la capacidad de hablar, y en general se adaptó a estar fuera del agua'/>
     </div>
   );
 }
-
 export default App;
---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../hojas-de-estilos/Testimonio.css'
+
+function Testimonio(props) {
+  return(
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio' 
+      src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} 
+      alt='Foto de Darwin'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> de {props.empresa}</p>
+        <p className='cargo-testimonio'>{props.cargo}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+export default Testimonio;
